7d088f4e91ae6353c466bdacd0a36eb98a1f5527
socegueda86 <socegueda86@gmail.com>
2022-09-24 01:09:16 -0500

    Create column_letter_to_number, user_input_verifier, is_space_in_column
    methods, the commit includes the tests for all of methods. They
    will be use as an input validation previous to placing a marker.

--- lib/4_in_line.rb
+++ lib/4_in_line.rb
@@ -1,5 +1,6 @@
 class Game
-  
+  attr_reader :board
+
   def initialize()
     @board = create_board()
   end
@@ -9,16 +10,35 @@ class Game
   end
 
   def draw_board()
-    row_separator = "+---+---+---+---+---+---+---+"
-    @board.each do |row|
+    row_separator = "  +---+---+---+---+---+---+---+"
+    @board.each_with_index do |row, i|
       puts row_separator
-      puts "| #{row[0].nil? ? " " : row[0] } | #{row[1].nil? ? " " : row[1] } | #{row[2].nil? ? " " : row[2] } | #{row[3].nil? ? " " : row[3] } | #{row[4].nil? ? " " : row[4] } | #{row[5].nil? ? " " : row[5] } | #{row[6].nil? ? " " : row[6] } |"
+      puts "#{6 - i} | #{row[0].nil? ? " " : row[0] } | #{row[1].nil? ? " " : row[1] } | #{row[2].nil? ? " " : row[2] } | #{row[3].nil? ? " " : row[3] } | #{row[4].nil? ? " " : row[4] } | #{row[5].nil? ? " " : row[5] } | #{row[6].nil? ? " " : row[6] } |"
       end
     puts row_separator
+    puts "    A   B   C   D   E   F   G  "
   end
-          
 
+  def place_marker(column, marker)
+    column = column_letter_to_number(column)
+  end
 
+  def column_letter_to_number(column)
+    hash_letter_to_numbers = {'A' => 1, 'B' => 2, 'C' => 3, 'D' => 4, 'E' => 5, 'F' => 6, 'G' => 7,}
+    hash_letter_to_numbers.fetch(column)
+  end
 
+  def user_input_verifier(user_input)  # this would check if the colums are among A - G, if the columns aren't full, if the values are appropiate 'A'-'G'
+    return false if user_input.length > 1 
+
+    user_input = user_input.upcase
+    return false if user_input.match(/[A-G]/).nil?
+    puts "Invalid input: please try again"
+    true
+  end
+
+  def is_space_in_column(user_input)
+    (0...6).any? {|i| @board[5-i][column_letter_to_number(user_input)].nil?}
+  end
 end
 
--- spec/4_in_line_spec.rb
+++ spec/4_in_line_spec.rb
@@ -3,12 +3,12 @@ require './lib/4_in_line.rb'
 describe Game do
   describe "#create_board" do
     it "creates board" do 
-      expect(subject.create_board).to eql([[nil, nil, nil, nil, nil, nil, nil],                                            
-        [nil, nil, nil, nil, nil, nil, nil],                                            
-        [nil, nil, nil, nil, nil, nil, nil],                                            
-        [nil, nil, nil, nil, nil, nil, nil],                                            
-        [nil, nil, nil, nil, nil, nil, nil],                                            
-        [nil, nil, nil, nil, nil, nil, nil]])
+      expect(subject.create_board).to eql([[nil, nil, nil, nil, nil, nil, nil],
+                                           [nil, nil, nil, nil, nil, nil, nil],
+                                           [nil, nil, nil, nil, nil, nil, nil],
+                                           [nil, nil, nil, nil, nil, nil, nil],
+                                           [nil, nil, nil, nil, nil, nil, nil],
+                                           [nil, nil, nil, nil, nil, nil, nil]])
     end
 
     it "total number of rows" do
@@ -24,5 +24,109 @@ describe Game do
     end
   end
 
+  describe "#place_marker" do
+    subject { Game.new }
+
+    xit "checks that the markers goes all the way down until it hits bottom" do
+        expect(subject.place_marker('A','X')).to eql([[nil, nil, nil, nil, nil, nil, nil],
+                                                      [nil, nil, nil, nil, nil, nil, nil],
+                                                      [nil, nil, nil, nil, nil, nil, nil],
+                                                      [nil, nil, nil, nil, nil, nil, nil],
+                                                      [nil, nil, nil, nil, nil, nil, nil],
+                                                      ['X', nil, nil, nil, nil, nil, nil]])
+    end
+
+    xit "checks that the markers goes all the way down until it hits another marker" do
+      subject.place_marker('A','X')
+      expect(subject.place_marker('A','X')).to eql([[nil, nil, nil, nil, nil, nil, nil],
+                                                    [nil, nil, nil, nil, nil, nil, nil],
+                                                    [nil, nil, nil, nil, nil, nil, nil],
+                                                    [nil, nil, nil, nil, nil, nil, nil],
+                                                    ['X', nil, nil, nil, nil, nil, nil],
+                                                    ['X', nil, nil, nil, nil, nil, nil]])
+    end
+
+    xit "checks that the markers goes all the way down until it hits another marker" do
+#2.tiomes {subject.place_marker('A', 'X')}  probably i should try to erase this, lets check if the subject its accumulating the markers or if every test its a new start
+      expect(subject.place_marker('E', 'X')).to eql([[nil, nil, nil, nil, nil, nil, nil],
+                                                     [nil, nil, nil, nil, nil, nil, nil],
+                                                     [nil, nil, nil, nil, nil, nil, nil],
+                                                     [nil, nil, nil, nil, nil, nil, nil],
+                                                     ['X', nil, nil, nil, nil, nil, nil],
+                                                     ['X', nil, nil, nil, 'X', nil, nil]])
+    end
+  end
+
+  describe '#column_letter_to_number' do
+
+    it "checks that the column A is correctly change in to number 1" do
+      expect(subject.column_letter_to_number('A')).to eql(1)
+    end
+
+    it "checks that the column F is correctly change in to number 6" do
+        expect(subject.column_letter_to_number('F')).to eql(6)
+    end
+
+    it "checks that the column C is correctly change in to number 3" do
+      expect(subject.column_letter_to_number('C')).to eql(3)
+    end
+
+    it "checks that the column G is correctly change in to number 7" do
+      expect(subject.column_letter_to_number('G')).to eql(7)
+    end
+  end
   
+  describe '#user_input_verifier' do 
+
+    it 'Accepts A as an inpunt' do
+      expect(subject.user_input_verifier('A')).to be true
+    end
+
+    it 'Accepts C as an inpunt' do
+      expect(subject.user_input_verifier('C')).to be true
+    end
+
+    it 'Accepts G as an inpunt' do
+      expect(subject.user_input_verifier('G')).to be true
+    end
+
+    it "Doesn't accepts numbers H as an inpunt" do
+      expect(subject.user_input_verifier('H')).to be false
+    end
+
+    it "Doesn't accepts numbers Z as an inpunt" do
+      expect(subject.user_input_verifier('Z')).to be false
+    end
+
+    it "Doesn't accepts numbers as an inpunt" do
+      expect(subject.user_input_verifier('1')).to be false
+    end
+
+    it "Doesn't accepts more than one character as an inpunt" do
+      expect(subject.user_input_verifier('ABC')).to be false
+    end
+
+    it "Doesn't accepts more than one character as an inpunt" do
+      expect(subject.user_input_verifier('1234')).to be false
+    end
+  end
+
+  describe '#is_space_in_column' do 
+
+    it  "Checks if the theres space in the column" do
+      expect(subject.is_space_in_column('A')).to be true
+    end
+
+    it  "Checks if the theres space in the column" do
+      # im filling the column with this line, cause the placing is not ready and function will depend on this one.
+      (0..5).each {|i| subject.board[i][1] = 'X'} 
+      expect(subject.is_space_in_column('A')).to be false
+    end
+
+    it "Checks if the theres space in the column" do
+      (0..4).each {|i| subject.board[5-i][1] = 'X'}
+      expect(subject.is_space_in_column('A')).to be true
+    end
+
+  end
 end
